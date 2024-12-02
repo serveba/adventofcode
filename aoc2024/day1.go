4282818732d3ff2d23608ceaf264f893a1bfb22f
Sergio Velasco <sergio.velasco@exponentiateam.com>
2024-12-02 10:04:11 +0100

    2024 Day 1 Part 2

--- aoc2024/day1.go
+++ aoc2024/day1.go
@@ -15,7 +15,7 @@ import (
 // Historian Hysteria
 func distanceSum(fileContent string) int {
 	sum := 0
-	leftList, rightList := extractLists(fileContent)
+	leftList, rightList := extractSortedLists(fileContent)
 	elements := len(leftList) // lists have the same length
 	for i := 0; i < elements; i++ {
 		sum += int(math.Abs(float64(leftList[i] - rightList[i])))
@@ -38,11 +38,37 @@ func extractLists(fileContent string) ([]int, []int) {
 		rightList = append(rightList, right)
 	}
 
+	return leftList, rightList
+}
+
+func extractSortedLists(fileContent string) ([]int, []int) {
+	leftList, rightList := extractLists(fileContent)
+
 	// sort the lists in ascending order
 	sort.Ints(leftList)
 	sort.Ints(rightList)
 
 	return leftList, rightList
+}
+
+func silimarityScores(fileContent string) int {
+	similarityScore := 0
+	leftList, rightList := extractSortedLists(fileContent)
+
+	for i := 0; i < len(leftList); i++ {
+		coef := 0
+		for j := 0; j < len(rightList); j++ {
+			if leftList[i] == rightList[j] {
+				coef += 1
+			}
+		}
+		if coef > 0 {
+			// fmt.Printf("%d appears %d times\n", leftList[i], coef)
+			similarityScore += leftList[i] * coef
+		}
+	}
+
+	return similarityScore
 }
 
 func Day1() {
@@ -52,5 +78,8 @@ func Day1() {
 	}
 
 	sum := distanceSum(string(byteArray))
-	fmt.Printf("Distance Sum: %d\n", sum)
+	fmt.Printf("Part 1: %d\n", sum)
+
+	similarityScore := silimarityScores(string(byteArray))
+	fmt.Printf("Part 2: %d\n", similarityScore)
 }
